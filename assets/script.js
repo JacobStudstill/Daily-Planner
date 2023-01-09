78099d2a7dc3530b5c62a09e4fdaa2a9c71a71d2
Jacob Studstill <jacob.studstill@gmail.com>
2023-01-09 10:08:36 -0500

    Information to be saved into local storage when the save button is pressed

--- assets/script.js
+++ assets/script.js
@@ -8,11 +8,35 @@ var timeUpdated = setInterval(function () {
   $('#currentDay').text(current)
 },1000);
 
-//Event Listener for the save button
+//Event Listener for the save button to add date to local storage
+$(".saveBtn").on("click", function(){
+  var time = $(this).parent().attr("id");
+  var info = $(this).siblings(".description").val();
+
+  console.log(time);
+  console.log(info);
+
+  localStorage.setItem(time,info);
+})
+
+$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+$("#hour-6 .description").val(localStorage.getItem("hour-6"));
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-18 .description").val(localStorage.getItem("hour-18"));
+$("#hour-19 .description").val(localStorage.getItem("hour-19"));
+$("#hour-20 .description").val(localStorage.getItem("hour-20"));
+$("#hour-21 .description").val(localStorage.getItem("hour-21"));
+$("#hour-22 .description").val(localStorage.getItem("hour-22"));
 $(function () {
-
-  var today = dayjs();
-
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
